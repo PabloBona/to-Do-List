c4a70a7adae273e5e19df10b52427e571a04218a
Anwar <anwarhussaini160@gmail.com>
2023-06-15 20:54:46 +0430

    Create Mock for the updateCompletedStatus

--- testing/toDoListPart2.test.js
+++ testing/toDoListPart2.test.js
@@ -1,9 +1,10 @@
 import { jest } from '@jest/globals';
 import removeAllCompleted from '../src/modules/removeAll.js';
+import updateCompletedStatus from '../src/modules/checkChange.js';
 
 describe('removeAllCompleted', () => {
   beforeEach(() => {
-    // Set up the DOM for each test if needed
+    // Arrange: Set up the DOM for each test if needed
     document.body.innerHTML = `
       <ul>
         <li class="completed">Task 1</li>
@@ -20,23 +21,45 @@ describe('removeAllCompleted', () => {
   });
 
   test('should remove all completed tasks and call filterCompleted', () => {
-    // Mock the listTaskClass object and the event
+    // Arrange: Mock the listTaskClass object and the event
     const listTaskClass = {
       filterCompleted: jest.fn(),
     };
     const event = { preventDefault: jest.fn() };
 
-    // Call the removeAllCompleted function
+    // Act: Call the removeAllCompleted function
     removeAllCompleted(listTaskClass, event);
 
-    // Verify that elements with the class 'completed' have been removed
+    // Assert: Verify that elements with the class 'completed' have been removed
     const completedList = document.querySelectorAll('.completed');
     expect(completedList.length).toBe(0);
 
-    // Verify that filterCompleted has been called
+    // Assert: Verify that filterCompleted has been called
     expect(listTaskClass.filterCompleted).toHaveBeenCalled();
 
-    // Verify that preventDefault has been called on the event
+    // Assert: Verify that preventDefault has been called on the event
     expect(event.preventDefault).toHaveBeenCalled();
+  });
+
+  describe('updateCompletedStatus', () => {
+    let li;
+    let task;
+    let listTaskClass;
+
+    beforeEach(() => {
+      // Arrange: Create a simulated li element
+      li = document.createElement('li');
+      li.id = 'task-1';
+
+      // Arrange: Create a simulated task object
+      task = {
+        completed: false,
+      };
+
+      // Arrange: Mock the listTaskClass object
+      listTaskClass = {
+        saveListToLocalStorage: jest.fn(),
+      };
+    });
   });
 });
